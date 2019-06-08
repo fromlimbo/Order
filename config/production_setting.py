3496af8131d86db5e9fc77ef7a0a52669399c0a1
mikeleung <llyhmy@qq.com>
2019-06-08 10:48:43 +0800

    Initial commit

---
+++ config/production_setting.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+DEBUG = True
+SQLALCHEMY_ECHO = True
+SQLALCHEMY_DATABASE_URI = 'mysql://root:@127.0.0.1/food_db?charset=utf8mb4' #'mysql://root:58plPi56SdYP8a63@127.0.0.1/food_db?charset=utf8mb4'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ENCODING = "utf8mb4"
+
+APP = {
+    'domain':'https://www.joyship.club'
+}
+
+RELEASE_VERSION="20180729001"
